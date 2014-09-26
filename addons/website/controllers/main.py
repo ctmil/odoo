fcaf801b2a8e1eea37c293cb4005ce8f3a8c9215
Cristian Sebastian Rocha <cristian.rocha@moldeo.coop>
2014-09-26 13:26:34 -0300

    Set INTRANET website

--- addons/website/controllers/main.py
+++ addons/website/controllers/main.py
@@ -24,6 +24,7 @@ logger = logging.getLogger(__name__)
 MAX_IMAGE_WIDTH, MAX_IMAGE_HEIGHT = IMAGE_LIMITS = (1024, 768)
 LOC_PER_SITEMAP = 45000
 SITEMAP_CACHE_TIME = datetime.timedelta(hours=12)
+INTRANET=True
 
 class Website(openerp.addons.web.controllers.main.Home):
     #------------------------------------------------------
@@ -52,6 +53,8 @@ class Website(openerp.addons.web.controllers.main.Home):
 
     @http.route('/page/<page:page>', type='http', auth="public", website=True)
     def page(self, page, **opt):
+        if INTRANET and request.uid == 3: return self.web_login()
+
         values = {
             'path': page,
         }
